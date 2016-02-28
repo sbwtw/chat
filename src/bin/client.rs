0da72f6590934e1e56ffa07f26b3e853f9dd55fd
石博文 <sbw@sbw.so>
2016-02-28 18:12:02 +0800

    use conrod to create gui

--- src/bin/client.rs
+++ src/bin/client.rs
@@ -1,53 +1,89 @@
 
-extern crate mio;
+#![feature(plugin)]
+#![plugin(clippy)]
 
-use mio::*;
-use mio::tcp::*;
+#[macro_use]
+extern crate conrod;
+extern crate piston_window;
 
-const CLIENT_TOKEN: Token = Token(0);
+use conrod::{
+    color,
+    Widget,
+    Canvas,
+    Text,
+    TextBox,
+    Theme,
+    Sizeable,
+    Colorable,
+    Positionable,
+};
+use piston_window::{EventLoop, Glyphs, PistonWindow, UpdateEvent, WindowSettings};
 
-struct Client {
-    stream: TcpStream,
-    remaining: Vec<Vec<u8>>,
+type Backend = (<piston_window::G2d<'static> as conrod::Graphics>::Texture, Glyphs);
+type Ui = conrod::Ui<Backend>;
+type UiCell<'a> = conrod::UiCell<'a, Backend>;
+
+struct App {
+    display: String,
 }
 
-impl Client {
-    fn ready_read(&mut self) {
+impl App {
+    fn new() -> App {
+        App {
+            //display: String::new(),
+            display: "aaaaaaaaaa".to_owned(),
+        }
     }
 
-    fn ready_write(&mut self) {
-    }
-}
+    fn set_widgets(&mut self, ui: &mut UiCell) {
 
-impl Handler for Client {
-    
-    type Timeout = ();
-    type Message = ();
+        Canvas::new()
+            .pad(8.0)
+            .color(color::WHITE)
+            .set(CANVAS, ui);
 
-    fn ready(&mut self, event_loop: &mut EventLoop<Client>, token: Token, events: EventSet) {
-        
-        if CLIENT_TOKEN != token || events.is_error() {
-            return;
-        }
+        Text::new(self.display.as_str())
+            //.middle()
+            //.up(0.0)
+            .color(color::RED)
+            .align_top_of(CANVAS)
+            .set(TEXT_AREA, ui);
 
-        if events.is_writable() && self.remaining.len() != 0 {
-            self.ready_write();
-        }
-
-        if events.is_readable() {
-            self.ready_read();
-        }
-
-        let event_set;
-
-        if self.remaining.len() != 0 {
-            event_set = EventSet::readable();
-        } else {
-            event_set = EventSet::readable() | EventSet::writable();
-        }
+        TextBox::new(&mut self.display)
+            .font_size(20)
+            .w_h(200.0, 50.0)
+            //.down_from(TEXT_AREA, 0.0)
+            .align_bottom_of(CANVAS)
+            .react(|_string: &mut String| {})
+            .set(INPUT_BOX, ui);
     }
 }
 
 fn main() {
-    println!("client");
+    let window: PistonWindow =
+        WindowSettings::new("main window", [400, 300])
+            .exit_on_esc(true).vsync(true).build().unwrap();
+
+    let mut ui = {
+
+        let theme = Theme::default();
+        let glyph_cache = Glyphs::new(&std::path::Path::new("/home/Projects/conrod/assets/fonts/NotoSans/NotoSans-Regular.ttf"), window.factory.borrow().clone());
+        Ui::new(glyph_cache.unwrap(), theme)
+    };
+
+    let mut app = App::new();
+
+    for event in window.ups(60) {
+        ui.handle_event(&event);
+
+        event.update(|_| ui.set_widgets(|mut ui| app.set_widgets(&mut ui)));
+        event.draw_2d(|c, g| ui.draw_if_changed(c, g));
+    }
 }
+
+widget_ids! {
+    CANVAS,
+    TEXT_AREA,
+    INPUT_BOX,
+}
+
